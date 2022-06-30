0b5b8c77408462db7f2e1b2f3df470c82af28f02
Carrot-01 <hliu98@outlook.com>
2022-06-30 15:54:20 -0400

    hello

---
+++ src/test/java/com/example/dummyapp/DummyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
